be4247a53b8f3fb5bdee456add87171b4fd925c3
astute-macro-4903 <christopher.williams5699@gmail.com>
2020-08-31 01:55:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,1 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
-def key_for_min_value(name_hash)
-
-end
+# prereqs: iterators, hashes, conditional logic# Given a hash with numeric values, return the key for the smallest valuedef key_for_min_value(name_hash)  if name_hash.empty?    return nil  endarr2 = []  name_hash.map do |x,y|    arr2 << y  endarr3 = arr2.sortkey = name_hash.find do |x,y|  y == arr3[0]endreturn key[0]end
